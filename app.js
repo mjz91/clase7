3897afd9a48d2c5ee1521935ffe8ed978b49fbad
mjz91 <martin.zanitto@gmai.com>
2021-07-22 18:40:53 -0300

    modifico README.md

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-function holaMundo(){
-    console.log('hola mundo!');
-}
+let fs = require('fs');
+let moment = require('moment');
+
+//console.log(fs);
+
+let datos = fs.readFileSync(__dirname + '/prueba.txt', 'utf-8');
+
+
+console.log(datos);
+
+console.log(moment().format('MM-DD-YY'));
